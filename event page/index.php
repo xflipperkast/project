d2ece4e2a1c8a83992a23484f40c766a75d2d7c5
RonaldLommers <94675327+RonaldLommers@users.noreply.github.com>
2022-05-31 10:20:32 +0200

    .

---
+++ event page/index.php
@@ -0,0 +1,103 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Language" content="en" >
+    <meta name="description" content="Green Dragon Energie">
+    <meta name="keywords" content="Green Dragon Energie">
+    <title>Green Dragon Energie</title>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/general_page.css">
+    <link rel="stylesheet" type="text/css" href="css/theme_footer.css">
+    <link rel="stylesheet" type="text/css" href="css/theme_style.css">
+  </head>
+  <body class="">
+    <div id="page-overlay" class="visible incoming">
+      <div class="loader-wrapper-outer">
+        <div class="loader-wrapper-inner">
+          <div class="lds-double-ring">
+            <div></div>
+            <div></div>
+            <div>
+              <div></div>
+            </div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <header class="header fixed-top" id="headerNav">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg ">
+          <a class="navbar-brand" href="#">
+            <img class="site-logo d-none" src="images/logo.png" alt="Webstie logo">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span><i class="fe fe-menu"></i></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item active">
+                <a class="nav-link js-scroll-trigger" href="#home">INFO</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#features">FAQ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="services">Contact</a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="services">event</a>
+              </li>
+            </ul> 
+            <div class="nav-item d-md-flex btn-login-signup">
+              <a class="link btn-login" href="login">Admin</a>
+              <a href="auth/signup" class="btn btn-pill btn-outline-primary sign-up">Sign_Up></a>
+              <a href="statistics" class="btn btn-pill btn-outline-primary btn-statistics text-uppercase">statistics</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    
+    <section class="banner"  id="home">
+      <div class="container">
+        <div class="row">
+            <div class="index">
+                <img class="draakje" src="images/draakje.png" alt="draakje">
+                <div class="animation-2">
+                  <img class="intro-img" src="images/blikjes.png" alt="blikjes">
+                   <div class="contents">
+              <h2 class="head-title">
+                WELKOM
+              </h2>
+              
+              <p class="ppp">
+                Green dragon is de nieuwste en sterkste energie drink van deze tijd met ons nieuwe recept is onze energie niet alleen powerfull maar ook laag in calorieën tijdens festivals of projecten big or small Green dragon can give it all.
+              </p>
+            </div>
+            </div>
+          </div>          
+        </div>
+        </div>
+    </section>
+    <div class="footer footer_top dark">
+      <div class="container m-t-60 m-b-50">
+        <div class="row">
+        </div>
+      </div>
+    </div>    
+  </body>
+</html>
