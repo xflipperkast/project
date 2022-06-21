b159f77c3ca46766ac074e64e443cdcf000b54a3
xflipperkast <94675097+xflipperkast@users.noreply.github.com>
2022-06-21 08:34:31 +0200

    3
    3

--- home.php
+++ home.php
@@ -1,105 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Versiebeheer 2.T1 LO1E</title>
-    <link href="css/home.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-  <section>
-      <a href="#" id="inloggen">inloggen</a>
-    <nav>
-      <img id="logo" src="img/logo.png" alt="logo">
-        <article id="nav">
-            <a href="#" class="nav" id="curent-page">Home</a>
-            <a href="#" class="nav">Producten</a>
-            <a href="#" class="nav">Evenementen</a>
-            <a href="#" class="nav">Contact</a>
-        </article>
-    </nav>
-  </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <section id="intro">
-    <article id="img">
-      <img src="img/blikjes.png">
-    </article>
-
-    <article id="text1">
-      <h1>Welkom</h1>
-      <p>Green dragon is de nieuwste en sterkste energie drink van deze tijd met 
+<link href="css/homepage.css" rel="stylesheet" type="text/css">
+<nav class="navbar navbar-expand-sm navbar-dark">
+    <img src="img/logo.png" width="200" alt=""> 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation"> 
+        <span class="navbar-toggler-icon"></span> 
+    </button>
+    <div class="end">
+        <div class="collapse navbar-collapse" id="navbarColor02">
+            <ul class="navbar-nav">
+            <li class="nav-item active"> <a class="nav-link mt-2" href="#" data-abc="true" id="clicked">Home<span class="sr-only">(current)</span></a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">producten</a> </li>
+                <li class="nav-item "> <a class="nav-link" href="#" data-abc="true">Evenementen</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#" data-abc="true">Contact</a> </li>
+				<li class="nav-item"> <a class="nav-link" href="#" data-abc="true">inloggen</a> </li>
+            </ul>        
+        </div>
+    </div>    
+</nav>
+<!-- Main Body -->
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5 col-md-6 col-12 pb-4">
+            <img src="img/blikjes.png">
+            </div>
+            <div class="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
+            <h1>Welkom</h1>
+       <p class="texts">Green dragon is de nieuwste en sterkste energie drink van deze tijd met 
         ons nieuwe recept is onze energie niet alleen powerfull maar ook laag in 
         calorieën tijdens festivals of projecten big or small Green dragon can give 
         it all.</p>
-        <img src="img/draakje.png" width="50%">
-    </article>
-  </section>
-</body>
-</html>
-<style>
-  #intro{
-    display: grid;
-    grid-template-columns: 20vw 50vw;
-    margin-top: 100px;
-    margin-left: 250px;
-  }
-  #text1{
-    color: white;
-    font-size: 230%;
-  }
-  #text1 img{
-    position: absolute;
-    z-index: -1;
-    opacity: 0.5;
-    top: 300px;
-  }
-/*
-  *{
-    margin: 0px;
-    padding: 0px;
-}
-
-#inloggen{
-    color: white;
-    float: right;
-    text-decoration: none;
-    font-size: 30px;
-    padding: 5px 25px 5px 25px;
-    border: 2px solid white;
-    border-radius: 30px;
-}
-nav{
-    background-color: black;
-    display: grid;
-    grid-template-columns: 40vw 60vw;
-    align-items: center;
-    position: relative;
-    top: 30px;
-}
-#logo{
-    width: 20%;
-}
-#nav{
-    display: flex;
-    column-gap: 40px;
-
-}
-.nav{
-    font-size: 40px;
-    text-decoration: none;
-    color: white;
-}
-#curent-page{
-    color: #a0e193;
-}
-a:hover{
-    text-decoration: underline #a0e193;
-}
-body{
-    background-color: black;
-}
-
-#intro{
-    margin-top: 1000px;
-}
-*/
-</style>
+        <img class="imgtextback" src="img/draakje.png" width="50%">
+            </div>
+        </div>
+    </div>
+</section>
